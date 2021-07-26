2165a5278a44dace33eacf83eba3ed7f78fd725a
BrianPHChen <brian770130@gmail.com>
2021-07-26 12:45:24 -0700

    remove redundant routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 const fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,6 +16,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
